a581690d49f9e514c45cd63ddd288e77a72a09ad
michal <michal.szostak.123@gmail.com>
2024-12-04 06:16:35 +0100

    Day 4

---
+++ 04_1.py
@@ -0,0 +1,47 @@
+from string_formatting import print_formatted
+from util import *
+import itertools as it
+
+DAY = 4
+
+answer = 0
+data = ''''''
+# data = load_test_data(DAY)
+data = load_data(DAY)
+
+print_formatted(f"&e3&#ec{data}")
+
+data = [i for i in data.splitlines()]
+print(data)
+
+# HORIZONTALS
+for row in data:
+    for x in range(len(row) - 3):
+        s = row[x:x+4]
+        if s in ["XMAS", "SAMX"]:
+            answer += 1
+
+# VERTICALS
+for x in range(len(data[0])):
+    for y in range(len(data) - 3):
+        s = "".join(data[y+i][x] for i in range(4))
+        if s in ["XMAS", "SAMX"]:
+            answer += 1
+
+# DIAGONALS >v
+for x in range(len(data[0]) - 3):
+    for y in range(len(data) - 3):
+        s = "".join(data[y+i][x+i] for i in range(4))
+        if s in ["XMAS", "SAMX"]:
+            answer += 1
+
+# DIAGONALS <^
+for x in range(len(data[0]) - 3):
+    for y in range(len(data) - 1, 2, -1):
+        # print(y, len(data))
+        s = "".join(data[y-i][x+i] for i in range(4))
+        if s in ["XMAS", "SAMX"]:
+            answer += 1
+
+print(answer)
+# pyperclip.copy(str(answer))
---
+++ 04_2.py
@@ -0,0 +1,26 @@
+from string_formatting import print_formatted
+from util import *
+import itertools as it
+
+DAY = 4
+
+answer = 0
+data = ''''''
+# data = load_test_data(DAY)
+data = load_data(DAY)
+
+print_formatted(f"&e3&#ec{data}")
+
+data = [i for i in data.splitlines()]
+print(data)
+
+# DIAGONALS >v
+for x in range(len(data[0]) - 2):
+    for y in range(len(data) - 2):
+        s1 = "".join(data[y+i][x+i] for i in range(3))
+        s2 = "".join(data[y+(2-i)][x+i] for i in range(3))
+        if s1 in ["MAS", "SAM"] and s2 in ["MAS", "SAM"]:
+            answer += 1
+
+print(answer)
+# pyperclip.copy(str(answer))
