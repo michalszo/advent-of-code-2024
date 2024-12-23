b48c30086de32eeff1c18650f376dbb1a07c1835
michal <michal.szostak.123@gmail.com>
2024-12-23 23:17:58 +0100

    Day 5 refactor

--- 05_1.py
+++ 05_1.py
@@ -8,47 +8,22 @@ answer = 0
 data = ''''''
 # data = load_test_data(DAY)
 data = load_data(DAY)
-
 print_formatted(f"&e3&#ec{data}")
 
 rules, pages = data.split("\n\n")
-rules = [[int(j) for j in i.split("|")] for i in rules.splitlines()]
-pages = [[int(j) for j in i.split(",")] for i in pages.splitlines()]
-print(rules, pages)
-
-# befores = {}
-# afters = {}
-# for n in range(100):
-#     before = {n}
-#     after = {n}
-#     changes = True
-#     while changes:
-#         changes = False
-#         for first, second in rules:
-#             for i in before.copy():
-#                 if i == second and first not in before:
-#                     before.add(first)
-#                     changes = True
-#             for i in after.copy():
-#                 if i == first and second not in after:
-#                     after.add(second)
-#                     changes = True
-#     befores[n] = before
-#     afters[n] = after
-#
-# print(befores)
+rules = frozenset(tuple(int(j) for j in i.split("|")) for i in rules.splitlines())
+pages = frozenset(tuple(int(j) for j in i.split(",")) for i in pages.splitlines())
+print(rules)
+print(pages)
 
 for page in pages:
     for a, b in it.combinations(page, 2):
-        if [b, a] in rules:
+        if (b, a) in rules:
             break
-    #     if (b in befores[a] and b not in afters[a]) or (a in afters[b] and not a in befores[b]):
-    #         print(page, a, b)
-    #         break
     else:
         answer += page[len(page) // 2]
         # print(a, b)
 
-print(answer)
-# 11979 nie
+# 7307
+print_formatted(f"&ffAnswer: &e2{answer}")
 # pyperclip.copy(str(answer))
--- 05_2.py
+++ 05_2.py
@@ -1,6 +1,7 @@
 from string_formatting import print_formatted
 from util import *
 import itertools as it
+from functools import cmp_to_key
 
 DAY = 5
 
@@ -8,50 +9,19 @@ answer = 0
 data = ''''''
 # data = load_test_data(DAY)
 data = load_data(DAY)
-
 print_formatted(f"&e3&#ec{data}")
 
 rules, pages = data.split("\n\n")
-rules = [[int(j) for j in i.split("|")] for i in rules.splitlines()]
-pages = [[int(j) for j in i.split(",")] for i in pages.splitlines()]
-print(rules, pages)
-
-# befores = {}
-# afters = {}
-# for n in range(100):
-#     before = {n}
-#     after = {n}
-#     changes = True
-#     while changes:
-#         changes = False
-#         for first, second in rules:
-#             for i in before.copy():
-#                 if i == second and first not in before:
-#                     before.add(first)
-#                     changes = True
-#             for i in after.copy():
-#                 if i == first and second not in after:
-#                     after.add(second)
-#                     changes = True
-#     befores[n] = before
-#     afters[n] = after
-#
-# print(befores)
-
-
-def check_page(_page):
-    for a, b in it.combinations(_page, 2):
-        if [b, a] in rules:
-            na, nb = _page.index(b), _page.index(a)
-            _page[na] = a
-            _page[nb] = b
-            return check_page(_page)
-    return _page
+rules = frozenset(tuple(int(j) for j in i.split("|")) for i in rules.splitlines())
+pages = frozenset(tuple(int(j) for j in i.split(",")) for i in pages.splitlines())
+print(rules)
+print(pages)
 
 for page in pages:
-    ordered_page = check_page(page.copy())
-    if ordered_page != page:
-        answer += ordered_page[len(ordered_page) // 2]
-print(answer)
-# 11979 nie
+    sorted_page = tuple(sorted(page, key=cmp_to_key(lambda a, b: ((b, a) in rules) - ((a, b) in rules))))
+    if sorted_page != page:
+        answer += sorted_page[len(sorted_page) // 2]
+
+# 4713
+print_formatted(f"&ffAnswer: &e2{answer}")
 # pyperclip.copy(str(answer))
